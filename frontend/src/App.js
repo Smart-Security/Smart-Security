09b5f3029dd9dfc71b90d76c40da2c4539b56625
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-15 20:00:30 +0100

    Configure the base routing in App Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom"
+import AdministrationPage from "./pages/administration/administration.page"
+import LoginPage from "./pages/login/login.page"
+import GoToPage from "./pages/goto/goto.page"
+import ErrorBoundary from "./components/error_boundary.component"
 
 function App() {
+
+  const navigate = useNavigate()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/" element={ <LoginPage /> } />
+        <Route path="login" element={ <LoginPage /> } />
+        <Route path="goto" element={ <GoToPage /> } />
+        <Route path="administration" element={ <AdministrationPage navigate={ navigate }/> } />
+        {/* Redirect to login if route is unknwon */}
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
+      </Routes>
+    </ErrorBoundary>
+    
   );
 }
 
