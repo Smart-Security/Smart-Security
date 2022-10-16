058db32a7a269357d8ae3a7e585ac4ccbeb2a437
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-16 19:01:22 +0100

    add strings and environment contants

---
+++ frontend/src/constants/enviroment.js
@@ -0,0 +1,6 @@
+/**
+ * This variable stores the base configuration that the application will use
+ */
+export const environment = {
+    endpoint: 'http://localhost:8080/',
+}
---
+++ frontend/src/constants/strings.js
@@ -0,0 +1,23 @@
+
+export const strings = {
+    companyName: "Smart Security",
+    login : "Login",
+    monitorization: "Monitorization",
+    goto: "Go To",
+    alarms: "Alarms",
+    users: "Users",
+    user: "User",
+    email: "Email address",
+    emailPlaceolder: "example@mail.com",
+    password: "Password",
+    passwordPlaceholder: "Password",
+    role: "Role",
+    floor: "Floor",
+    groundFloor: "Ground Floor",
+    leaveBuilding: "Leave Building",
+    logout: "Logout",
+    signin: "Sign In",
+    managenment: "Managenment",
+    adduser: "Add User",
+    add: "Add",
+}
