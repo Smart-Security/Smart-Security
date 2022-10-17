53975d464f2e0e3d6c11bf40b7d8965214356f30
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-17 23:38:01 +0100

    setup default theme by system prefrence

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,45 @@
 import './App.css';
+import React from 'react'
 import { Routes, Route, Navigate } from "react-router-dom"
 import AdministrationPage from "./pages/administration/administration.page"
 import LoginPage from "./pages/login/login.page"
 import GoToPage from "./pages/goto/goto.page"
 import ErrorBoundary from "./components/error_boundary.component"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+
+  // initialize theme 
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <ErrorBoundary>
-      <Routes>
-        <Route path="/" element={ <LoginPage /> } />
-        <Route path="login" element={ <LoginPage /> } />
-        <Route path="goto" element={ <GoToPage /> } />
-        <Route path="administration" element={ <AdministrationPage /> } />
-        {/* Redirect to login if it is an unknwon route */}
-        <Route
-          path="*"
-          element={<Navigate to="/" replace />}
-        />
-      </Routes>
-    </ErrorBoundary>
-    
+    <ThemeProvider theme={theme}>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={ <LoginPage /> } />
+          <Route path="login" element={ <LoginPage /> } />
+          <Route path="goto" element={ <GoToPage /> } />
+          <Route path="administration" element={ <AdministrationPage /> } />
+          {/* Redirect to login if it is an unknwon route */}
+          <Route
+            path="*"
+            element={<Navigate to="/" replace />}
+          />
+        </Routes>
+        <CssBaseline />
+      </ErrorBoundary>
+    </ThemeProvider>
   );
 }
 
