7924b16c04952409b098644dec875da81098d2c9
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-23 18:04:27 +0100

    added alarms base component

---
+++ frontend/src/pages/administration/components/alarms/alarms.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import strings from './../../../../constants/strings'
+import "./alarms.component.css"
+
+export default function Alarms(props) {
+    return <h1 className="content-title">{strings.adminstration.alarms.title}</h1>
+}
