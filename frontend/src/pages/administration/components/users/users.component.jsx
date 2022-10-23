85c9cc5218de67e0833718b4b7c63ab2e9aa9768
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-23 18:02:53 +0100

    added useras management base component

---
+++ frontend/src/pages/administration/components/users/users.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import strings from './../../../../constants/strings'
+import "./users.component.css"
+
+
+export default function Users(props) {
+    return <h1 className="content-title">{strings.adminstration.users.title}</h1>
+}
