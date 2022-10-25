3513d488bf0a80cc1539d3974927a4d941659626
1996joaocosta <1996joaocosta@gamil.com>
2022-10-25 12:28:15 +0100

    creation of dialog component

--- frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
+++ frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
@@ -1,6 +1,23 @@
-
-import React from 'react';
+import React from "react";
+import strings from "../../../../../../constants/strings";
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
 
 export default function AddUser(user) {
-    return <h1>Ola mundo</h1>
+  return <h1>Ola mundo</h1>;
+}
+
+export function AddUserDialog(props) {
+  const { onClose, open } = props;
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>{strings.adminstration.users.adduser}</DialogTitle>
+      <AddUser />
+    </Dialog>
+  );
 }
