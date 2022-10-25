44f36ae6f3ef826261d39a63ceb5d22594154bda
1996joaocosta <1996joaocosta@gamil.com>
2022-10-25 12:46:57 +0100

    load the building information on init

--- frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
+++ frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../../../../../hooks/use-auth.hook";
+import UserManagementService from "../../../../../../services/users.management.service";
 import strings from "../../../../../../constants/strings";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
 
-export default function AddUser(user) {
+export default function AddUser(props) {
+  const auth = useAuth();
+  const [building, setBuilding] = useState(null);
+
+  // when component loads for the first time, load
+  useEffect(() => {
+    /**
+     * Request the api to provide the information of the build floor composition.
+     */
+    const loadBuilding = async () => {
+      try {
+        const response = await UserManagementService.getBuilding(auth.user);
+        setBuilding(response.data);
+      } catch (e) {
+        // if (props.onError) props.onError(e)
+        // todo show message
+        console.error(e);
+      }
+    };
+    loadBuilding();
+  }, []);
   return <h1>Ola mundo</h1>;
 }
 
