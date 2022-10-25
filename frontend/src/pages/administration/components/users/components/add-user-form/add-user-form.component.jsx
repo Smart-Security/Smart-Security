f232d2c185950f1b90194964157fd5aa04ab4a8e
1996joaocosta <1996joaocosta@gamil.com>
2022-10-25 17:23:46 +0100

    Added components to the "Add User" form

--- frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
+++ frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
@@ -4,10 +4,22 @@ import UserManagementService from "../../../../../../services/users.management.s
 import strings from "../../../../../../constants/strings";
 import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
+import { FormControl, TextField } from "@mui/material";
+import Box from "@mui/material/Box";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import moment from "moment";
+import MultipleSelectChip from "./multiple-selection-chip.component";
 
 export default function AddUser(props) {
   const auth = useAuth();
+
   const [building, setBuilding] = useState(null);
+  const [value, setValue] = useState(moment());
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // when component loads for the first time, load
   useEffect(() => {
@@ -26,7 +38,39 @@ export default function AddUser(props) {
     };
     loadBuilding();
   }, []);
-  return <h1>Ola mundo</h1>;
+
+  const handleBirthDateChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box>
+      <TextField required label={strings.adminstration.users.name} />
+      <TextField required label={strings.adminstration.users.age} />
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <DesktopDatePicker
+          label={strings.adminstration.users.birthdate}
+          inputFormat="MM/DD/YYYY"
+          value={value}
+          onChange={handleBirthDateChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+      <TextField
+        required
+        label={strings.login.email}
+        inputProps={{ inputMode: "email" }}
+        onChange={(newValue) => setEmail(newValue.target.value)}
+      />
+      <TextField
+        required
+        label={strings.login.password}
+        type="password"
+        onChange={(newValue) => setPassword(newValue.target.value)}
+      />
+      <MultipleSelectChip />
+    </Box>
+  );
 }
 
 export function AddUserDialog(props) {
