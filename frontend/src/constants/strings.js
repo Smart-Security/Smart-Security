fb4bc9d56ae73c44129e186e008bb63363865cbf
1996joaocosta <1996joaocosta@gamil.com>
2022-10-25 17:19:06 +0100

    Added new strings

--- frontend/src/constants/strings.js
+++ frontend/src/constants/strings.js
@@ -1,48 +1,52 @@
-
 const strings = {
-    companyName: "Smart Security",
-    monitorization: "Monitorization",
-    user: "User",
-    floor: "Floor",
-    groundFloor: "Ground Floor",
-    leaveBuilding: "Leave Building",
-    logout: "Logout",
-    login: {
-        title: "Login",
-        email: "Email address",
-        emailPlaceolder: "example@mail.com",
-        password: "Password",
-        passwordPlaceholder: "Password",
-        signin: "Sign In",
-        invalidEmail: "Invalid email address",
-        invalidPassword: "Invalid password",
-        invalidCredentials: "Invalid email or password"
+  companyName: "Smart Security",
+  monitorization: "Monitorization",
+  user: "User",
+  floor: "Floor",
+  divisions: "Divisions",
+  groundFloor: "Ground Floor",
+  leaveBuilding: "Leave Building",
+  logout: "Logout",
+  login: {
+    title: "Login",
+    email: "Email address",
+    emailPlaceolder: "example@mail.com",
+    password: "Password",
+    passwordPlaceholder: "Password",
+    signin: "Sign In",
+    invalidEmail: "Invalid email address",
+    invalidPassword: "Invalid password",
+    invalidCredentials: "Invalid email or password",
+  },
+  goto: {
+    title: "Go To",
+  },
+  adminstration: {
+    managenment: "Managenment",
+    users: {
+      title: "Users",
+      role: "Role",
+      adduser: "Add User",
+      add: "Add",
+      name: "Name",
+      age: "Age",
+      birthdate: "Birth Date",
     },
-    goto: {
-        title: "Go To",
+    monitorization: {
+      title: "Monitorization",
     },
-    adminstration: {
-        managenment: "Managenment",
-        users: {
-            title: "Users",
-            role: "Role",
-            adduser: "Add User",
-            add: "Add",
-        },
-        monitorization: {
-            title: "Monitorization",
-        },
-        alarms: {
-            title: "Alarms"
-        }
+    alarms: {
+      title: "Alarms",
     },
-    exceptions: {
-        forbiden: "You have no access to this area.",
-        unauthorized: "You are not authorized to access this area.",
-        notFound: "Page Not Found",
-        networkError: "Unable to reach the server. Check your network connection.",
-        generic: "Something went wrong. Try deativate alarms manually or talk with the Security Guard"
-    }
-}
+  },
+  exceptions: {
+    forbiden: "You have no access to this area.",
+    unauthorized: "You are not authorized to access this area.",
+    notFound: "Page Not Found",
+    networkError: "Unable to reach the server. Check your network connection.",
+    generic:
+      "Something went wrong. Try deativate alarms manually or talk with the Security Guard",
+  },
+};
 
 export default strings;
