fbb6c5f0bf9c30809c89db488048e35a9970e382
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-26 12:24:28 +0100

    users component with backend pagination

--- frontend/src/pages/administration/components/users/users.component.jsx
+++ frontend/src/pages/administration/components/users/users.component.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 import strings from './../../../../constants/strings'
 import "./users.component.css"
 import { DataGrid } from '@mui/x-data-grid';
@@ -6,154 +7,86 @@ import Fab from '@mui/material/Fab';
 import NavigationIcon from '@mui/icons-material/Navigation';
 import { useTheme } from '@mui/material/styles';
 import Zoom from '@mui/material/Zoom';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-// import { createFakeServer } from '@mui/x-data-grid-generator';
-
-// const { columns, initialState, useQuery } = createFakeServer({}, SERVER_OPTIONS);
+import UserManagementService from './../../../../services/users.management.service.js' 
+import { useAuth } from './../../../../hooks/use-auth.hook'
+import { ROLETYPE } from '../../../../models/authentication.model';
+import { DIVISION_TYPE } from './../../../../models/divisions-type.model'
 
 const columns = [
-    { field: 'name', headerName: 'Name', width: 300},
-    { field: 'email', headerName: 'Email', width: 300 },
-    { 
-        id : "divisions",
-        field: 'divisionDtos', headerName: 'Divisions', width: 300, sortable: false, 
-        renderCell: (divisions) => {
-            return (
-                <Stack direction="row" spacing={1}>
-                    {
-                        divisions.row.divisionDtos.map((division, index) => <Chip key={`${divisions.name + ""}`} label={division.name} />)
-                    }
-                </Stack>
-            );
-        }
-    },
-];
-  
-const rows = [
     {
-        id: "f045b8b0-788a-4415-a45c-a8f0193d288e",
-        uuid: "f045b8b0-788a-4415-a45c-a8f0193d288e",
-        email: "example@mail.com",
-        name: "aa",
-        age: 10,
-        divisionDtos: [
-            {
-                id: "123",
-                name: "Lab 2"
-            },
-            {
-                id: "128",
-                name: "Lab 3"
-            },
-            {
-                id: "123",
-                name: "Lab 4"
-            }
-        ]
+      field: 'name', headerName: strings.adminstration.users.list.name, width: 300
     },
     {
-        id: "9c95f89f-e8ec-4e2d-a2f0-a141f2c74a8b",
-        uuid: "9c95f89f-e8ec-4e2d-a2f0-a141f2c74a8b",
-        email: "dani@gmail.com",
-        name: "aa",
-        age: 10,
-        divisionDtos: [
-            {
-                name: "Lab 2"
-            },
-            {
-                name: "Lab 3"
-            },
-            {
-                name: "Lab 4"
-            }
-        ]
+      field: 'email', headerName: strings.adminstration.users.list.email, width: 300
     },
     {
-        id: "7d8a9c92-454d-4aff-be9f-7b827f90111a",
-        uuid: "7d8a9c92-454d-4aff-be9f-7b827f90111a",
-        email: "daniel@gmail.com",
-        name: "Daniel Fernandes",
-        age: 26,
-        divisionDtos: [
-            {
-                name: "Lab 2"
-            },
-            {
-                name: "Lab 3"
-            },
-            {
-                name: "Lab 4"
-            },
-            {
-                name: "Lab 4"
-            },
-            {
-                name: "Lab 4"
-            },
-        ]
-    }
-];
+      field: 'role', headerName: strings.adminstration.users.list.role, width: 250,  valueGetter: (params) => params.row.role === ROLETYPE.ADMIN ? strings.roles.admin : strings.roles.user
+    },
+    {
+      field: 'divisions', headerName: strings.adminstration.users.list.access, width: 250,  valueGetter: (params) => params.row.divisions.filter((division) => division.type !== DIVISION_TYPE.common_area).map((division) => { return division.name }).join(', ')
+    },
+]
 
 export default function Users(props) {
-    
+
+    const auth = useAuth()
     const theme = useTheme();
+
+    const [pageState, setPageState] = useState({
+        isLoading: false,
+        data: [],
+        total: 0,
+        page: 0,
+        pageSize: 5
+    })
     
-    const [page, setPage] = useState(0);
-    const [pageSize, setPageSize] = useState(5)
+    useEffect(() => {
 
-    // const queryOptions = React.useMemo(
-    //     () => ({
-    //       page,
-    //       pageSize,
-    //     }),
-    //     [page, pageSize],
-    // );
+        const fetchData = async () => {
+            
+          try {
+            setPageState(old => ({ ...old, isLoading: true }))
 
-    //const { isLoading, data, pageInfo } = useQuery(queryOptions);
+            const response = await UserManagementService.listUsers(auth.user, pageState.page, pageState.pageSize)
+            const total = response.data.maxUsers
+            const users = await response.data.users
 
-    // Some API clients return undefined while loading
-    // Following lines are here to prevent `rowCountState` from being undefined during the loading
-    // const [rowCountState, setRowCountState] = React.useState(
-    //     pageInfo?.totalRowCount || 0,
-    // );
+            setPageState(old => ({ ...old, isLoading: false, data: users, total: total }))
+          } catch (err) {
+            // TODO tratar excecoes
+          }
+          
+        }
 
-    // React.useEffect(() => {
-    //     setRowCountState((prevRowCountState) =>
-    //       pageInfo?.totalRowCount !== undefined
-    //         ? pageInfo?.totalRowCount
-    //         : prevRowCountState,
-    //     );
-    // }, [pageInfo?.totalRowCount, setRowCountState]);
+        fetchData()
+      }, [pageState.page, pageState.pageSize])
 
     const transitionDuration = {
         enter: theme.transitions.duration.enteringScreen,
         exit: theme.transitions.duration.leavingScreen,
     };
-    const handleChangeRowsPerPage = (event) => {
-        //setRowsPerPage(+event.target.value);
-        // setPage(0);
-    };
 
     return (
         <div className="users-container">
             <h1 className="content-title">{strings.adminstration.users.title}</h1>
-            <div className="users-data-table-container">
+                <div className="users-data-table-container">
                     <DataGrid
-                        loading={true}
-                        rows={rows}
-                        columns={columns}
-                        pageSize={pageSize}
-                        count={3}
-                        rowsPerPageOptions={[10, 25, 100]}
-                        onRowsPerPageChange={handleChangeRowsPerPage}
-                        onPageChange={(newPage) => setPage(newPage)}
-                        onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                        autoHeight
                         checkboxSelection
+                        rows={pageState.data}
+                        rowCount={pageState.total}
+                        loading={pageState.isLoading}
+                        rowsPerPageOptions={[5, 10, 30]}
+                        pagination
+                        page={pageState.page}
+                        pageSize={pageState.pageSize}
                         paginationMode="server"
+                        onPageChange={(newPage) => {setPageState(old => ({ ...old, page: newPage }))}}
+                        onPageSizeChange={(newPageSize) => setPageState(old => ({ ...old, pageSize: newPageSize }))}
+                        columns={columns}
+                        getRowId={(row => row.uuid)}
                     />
-            </div>
+                </div>
             <Zoom
                 in={true}
                 timeout={transitionDuration}
