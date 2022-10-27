0b6e7707ec0263ccf174a7c60cb38d74b4ab671f
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-27 11:38:00 +0100

    added model ALARM_STATES

---
+++ frontend/src/models/alarm-state.model.js
@@ -0,0 +1,8 @@
+export const ALARM_STATES = {
+    activate: "ACTIVATE",
+    deactivate: "DEACTIVATE",
+    keepActivate: "KEEP_ACTIVATE",
+    keepDeactivate: "KEEP_DEACTIVATE",
+    deactivateBySecurityGuard: "DEACTIVATE_BY_SECURITY_GUARD",
+    activateBySecurityGuard: "ACTIVATE_BY_SECURITY_GUARD",
+};
