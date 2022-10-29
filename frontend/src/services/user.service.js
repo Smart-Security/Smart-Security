23efa480162c64800ceabe47fdeb7bab74f8416c
Henrique Marques <henrique.j.nunesmarques@gmail.com>
2022-10-30 00:43:31 +0100

    updated user service with goto api

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -2,43 +2,68 @@ import environment from "../constants/enviroment";
 import apiService from "./api.service";
 
 export default class UserService {
-
     static endpoints = {
         login: "auth/login",
         register: "auth/register",
-        profile: "api/fo/users"
+        profile: "api/fo/users",
+        goto: "api/fo/users/goto",
     };
 
     /**
      * User authentication
-     * @param {Authentication} authentication 
+     * @param {Authentication} authentication
      * @returns
      */
     static async login(authentication) {
-        return await apiService.post(environment.baseUrl + this.endpoints.login, authentication)
+        return await apiService.post(
+            environment.baseUrl + this.endpoints.login,
+            authentication
+        );
     }
 
     /**
      * Register a new user
-     * @param {string} email 
-     * @param {string} password 
-     * @returns 
+     * @param {string} email
+     * @param {string} password
+     * @returns
      */
     static async register(email, password) {
-        return await apiService.post(environment.baseUrl + this.endpoints.login, {
-            email: email,
-            password: password,
-        })
+        return await apiService.post(
+            environment.baseUrl + this.endpoints.login,
+            {
+                email: email,
+                password: password,
+            }
+        );
     }
-    
+
     /**
      * Loads the user logged information
      * @param {*} user logged user
-     * @returns 
+     * @returns
      */
     static async profileInformation(user) {
-        return await apiService.get(environment.baseUrl + this.endpoints.profile, {
-            headers: { 'Authorization' : `${user.tokenType} ${user.token}` }
-        })
+        return await apiService.get(
+            environment.baseUrl + this.endpoints.profile,
+            {
+                headers: { Authorization: `${user.tokenType} ${user.token}` },
+            }
+        );
+    }
+
+    /**
+     * Go to a room
+     * @param {*} auth logged information
+     * @param {*} uuidList division the user want to go
+     * @returns
+     */
+    static async goto(auth, uuidList) {
+        return await apiService.post(
+            environment.baseUrl + this.endpoints.goto,
+            { uuids: uuidList },
+            {
+                headers: { Authorization: `${auth.tokenType} ${auth.token}` },
+            }
+        );
     }
 }
