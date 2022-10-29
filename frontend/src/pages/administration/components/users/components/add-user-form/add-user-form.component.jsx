44340f501dc6a339ef2ce4a1e2e6fbff2024da39
jorhug <102927982+jorhug@users.noreply.github.com>
2022-10-29 16:49:15 +0100

    created test mode and user load for edit user created function to change and validate input fields on edit user form created function to create and request the edition

--- frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
+++ frontend/src/pages/administration/components/users/components/add-user-form/add-user-form.component.jsx
@@ -13,18 +13,56 @@ import moment from "moment";
 import MultipleSelectChip from "./multiple-selection-chip.component";
 import { KNOWHTTPSTATUS } from "./../../../../../../services/api.service";
 import AddUser from "./../../../../../../models/add-user.model";
-import { CircularProgress } from "@mui/material";
+import EditUser from "./../../../../../../models/edit-user.model";
 import { Skeleton } from "@mui/material";
 
 export default function AddUserForm(props) {
   const auth = useAuth();
+  const editMode = "edit";
 
   const [building, setBuilding] = useState(null);
   const [name, setName] = useState("");
   const [birthdate, setBirthDate] = useState(moment());
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [divisionsSelected, setDivisionsSelected] = useState(null);
+  const [divisionsSelected, setDivisionsSelected] = useState([]);
+
+  //props.mode = "edit";
+  const modeTest = true;
+  const user = {
+    uuid: "a2e6b048-3cc0-4ae1-b287-0b7ed2d38f26",
+    email: "hugo@gmail.com",
+    name: "Hugo Jorge",
+    birthDate: "2020-04-19T23:00:00.000+00:00",
+    active: true,
+    divisions: [
+      {
+        uuid: "292bc745-ec85-42ff-b927-d263172f58b1",
+        type: "COMMON_AREA",
+        name: "Corredor",
+        on: true,
+      },
+      {
+        uuid: "f1ff6349-57bf-4628-bc31-c70fbd2d475f",
+        type: "COMMON_AREA",
+        name: "LAS",
+        on: true,
+      },
+      {
+        uuid: "6fa77c66-f876-4959-8665-5407ba3fafaa",
+        type: "OFFICE",
+        name: "LIS",
+        on: true,
+      },
+      {
+        uuid: "d7780df3-1d0e-4182-9e6e-183655e48175",
+        type: "COMMON_AREA",
+        name: "Entrada",
+        on: true,
+      },
+    ],
+    role: "ROLE_USER",
+  };
 
   // builds the base strucure to the
   const validationReturn = (result, message) => {
@@ -59,8 +97,28 @@ export default function AddUserForm(props) {
         console.error(e);
       }
     };
+
+    // load building configurations
     loadBuilding();
+
+    // if the form is on
+    if (/* props.mode === editMode */ modeTest)
+      fillInputsOnEdit(/* props.user */ user);
   }, []);
+
+  /**
+   * Fill the inputs with user data
+   */
+  const fillInputsOnEdit = (user) => {
+    const divisionsUuids = user.divisions.filter(
+      (division) => division.type !== "COMMON_AREA"
+    );
+
+    setEmail(user.email);
+    setName(user.name);
+    setBirthDate(user.birthDate);
+    setDivisionsSelected(divisionsUuids);
+  };
 
   /**
    * when email changes validate the email and update the email error state
@@ -128,35 +186,61 @@ export default function AddUserForm(props) {
    */
   useEffect(() => {
     const isDivisionsEmpty = () => {
-      const divisionsEmpty = divisionsSelected == null;
+      const divisionsIsNotEmpty = divisionsSelected.length > 0;
       return validationReturn(
-        divisionsEmpty,
-        divisionsEmpty ? "" : "Select divisions"
+        divisionsIsNotEmpty,
+        divisionsIsNotEmpty ? "" : strings.adminstration.users.selectDivisions
       );
     };
+
     setDivisionsValidationResult(isDivisionsEmpty());
   }, [divisionsSelected]);
 
-  // Check if form is valid (password and email completed)
-  const isFormNotValid =
-    !emailValidationResult.result || !passwordValidationResult.result;
-
+  // Check if edit or add form is valid (name, divisions, password and email completed)
+  const isFormNotValid = () => {
+    if (/* props?.mode === editMode */ modeTest) {
+      return !nameValidationResult.result;
+    }
+    return (
+      !nameValidationResult.result ||
+      !emailValidationResult.result ||
+      !passwordValidationResult.result ||
+      !divisionsValidationResult.result
+    );
+  };
   /**
    * When login form is submited and the fields are valid.
    */
   const onFormSubmit = async () => {
     try {
-      // Create the user using the email, name, birthdate, password and the divisions selected uuids
-      const user = new AddUser(
-        email,
-        name,
-        birthdate,
-        password,
-        //Get the selected divisions uuid's
-        divisionsSelected.map((division) => division.uuid)
-      );
+      if (/* props?.mode === editMode */ modeTest) {
+        // Edits the user using the email, name, birthdate and the divisions selected uuids
+        const user = new EditUser(
+          email,
+          name,
+          birthdate,
+          //Get the selected divisions uuid's
+          divisionsSelected.map((division) => division.uuid)
+        );
 
-      const response = await UserManagementService.addUser(auth.user, user);
+        const response = await UserManagementService.editUser(
+          auth.user,
+          user,
+          props.user.uuid
+        );
+      } else {
+        // Create the user using the email, name, birthdate, password and the divisions selected uuids
+        const user = new AddUser(
+          email,
+          name,
+          birthdate,
+          password,
+          //Get the selected divisions uuid's
+          divisionsSelected.map((division) => division.uuid)
+        );
+
+        const response = await UserManagementService.addUser(auth.user, user);
+      }
     } catch (e) {
       // if status code is unauthorized the user credentials are wrong
       if (e?.response?.status === KNOWHTTPSTATUS.unauthorized)
@@ -171,6 +255,7 @@ export default function AddUserForm(props) {
       <TextField
         required
         label={strings.adminstration.users.name}
+        value={name}
         onChange={(newValue) => setName(newValue.target.value)}
         error={!nameValidationResult.result}
         helperText={nameValidationResult.message}
@@ -188,21 +273,33 @@ export default function AddUserForm(props) {
         required
         label={strings.login.email}
         inputProps={{ inputMode: "email" }}
+        value={email}
+        disabled={/* props?.mode === editMode */ modeTest}
         onChange={(newValue) => setEmail(newValue.target.value)}
         error={!emailValidationResult.result}
         helperText={emailValidationResult.message}
       />
-      <TextField
-        required
-        label={strings.login.password}
-        type="password"
-        onChange={(newValue) => setPassword(newValue.target.value)}
-        error={!passwordValidationResult.result}
-        helperText={passwordValidationResult.message}
-      />
+      {
+        /* props?.mode === editMode */ modeTest ? (
+          <React.Fragment></React.Fragment>
+        ) : (
+          <TextField
+            required
+            label={strings.login.password}
+            type="password"
+            onChange={(newValue) => setPassword(newValue.target.value)}
+            error={!passwordValidationResult.result}
+            helperText={passwordValidationResult.message}
+          />
+        )
+      }
+
       {building ? (
         <MultipleSelectChip
           divisions={building.floorDtos}
+          selectedDivisions={divisionsSelected}
+          error={!divisionsValidationResult.result}
+          helperText={divisionsValidationResult.message}
           onSelectedDivisions={(selectedDivisions) =>
             setDivisionsSelected(selectedDivisions)
           }
@@ -211,7 +308,7 @@ export default function AddUserForm(props) {
         <Skeleton variant="rounded" height={60} sx={{ m: 1, width: 300 }} />
       )}
       <Button
-        disabled={isFormNotValid}
+        disabled={isFormNotValid()}
         type="submit"
         className="sign-in-btn"
         variant="contained"
